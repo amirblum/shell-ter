747d54a3f615e2147cf2ee8e3f3541c3c124090b
Amir Blum <amirblum@gmail.com>
2020-02-01 16:58:27 +0200

    Player controls UI

---
+++ Assets/UIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject _startScreen;
+    [SerializeField] GameObject _endScreen;
+
+    protected void Awake()
+    {
+        _startScreen.SetActive(true);
+        _endScreen.SetActive(false);
+    }
+}
